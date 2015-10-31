8ba02fbf5f95c55e0e3b43541ba7c2351f55787f
mmmfang <mae.fang@gmail.com>
2015-10-31 01:39:32 -0400

    add text animation

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# api-movie-grabber
+# api-movie-grabber aka Random Movie Generator
 
-This is my first time using an API. I used OMDB to grab all the movie data and display movies as posters.
+This is my first time using an API. I used OMDB to grab all movie data from OMDB.
+Only movies with posters are displayed, and you can click on the posters to go to IMDB and learn more about the movie. Added an animation to the h1.
--- test.js
+++ test.js
@@ -12,9 +12,12 @@ $(function(){
     promise.done(function(data) {
       console.log(data);
       $('ul').html('');
+       // $.unique(data.Search); //tried this Jquery method but this only works on dom elements so didn't work on my array of objects! leaving it in here to remind me there is this property
+       
        data.Search.forEach(function (movie) {
         if (movie.Poster!== "N/A"){
-         $('ul').append('<a href="http://www.imdb.com/title/'+movie.imdbID+'/"> <img src="'+movie.Poster+' " /> </a>') 
+
+         $('ul').append('<a href="http://www.imdb.com/title/'+movie.imdbID+'/"><img src="'+movie.Poster+' " /></a>') 
         }
         // else {
         //  $('ul').append('<li> Movie Title: '+ movie.Title + '</li>');            
